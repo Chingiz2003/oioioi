3467accae09af1d73d6d59ce04bed7032880b6c2
Chingiz Shalbaev <chingiz605@gmail.com>
2023-01-31 21:36:29 +0600

    First commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from main import views
+from django.http import HttpResponse
+
+urlpatterns = [
+    path('', views.index),
+    path('login', views.login),
+    path('index', views.index),
+]
