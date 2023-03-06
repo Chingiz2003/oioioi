27acece77fc2f052e04522c5c49a881b03302e31
Chingiz Shalbaev <chingiz605@gmail.com>
2023-03-06 09:33:32 +0600

    First commit

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from main import views
+from .views import *
 from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index),
-    path('login', views.login),
-    path('index', views.index),
+    path('registr/', registr, name='registr'),
+    path('success/', success, name='success'),
+    path('', index, name='home'),
+    path('index/', index, name='index'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('proba/', proba, name='proba'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_done.html"), name="password_reset_complete"),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'main',
 ]
 
@@ -118,8 +119,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'atkushi09@gmail.com' 
+EMAIL_HOST_PASSWORD = 'tqjlogzibfgejidb' 
+
+LOGIN_REDIRECT_URL = '/login/'
+LOGIN_URL = '/login/'
+
+
+
+
